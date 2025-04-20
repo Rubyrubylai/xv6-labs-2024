23e5c611800ec5a47a332aa4ffe1406b3cd08bcd
Rubyrubylai <r844312@gmail.com>
2025-04-20 18:34:21 +0800

    feat: attack

--- user/attack.c
+++ user/attack.c
@@ -6,8 +6,20 @@
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+  if(argc != 1){
+    exit(1);
+  }
 
+  char *mem = sbrk(PGSIZE * 32);
+
+  for (int i = 0; i < PGSIZE * 32; i++) {
+    if (memcmp(mem + i, "my very very very secret pw is:   ", 34) == 0) {
+      write(2, mem + i + 32, 8); // TODO: 不知道為什麼印不出來
+      write(2, "\n", 1);
+      exit(0);
+    }
+  }
+
+  write(2, "secret not found\n", 18);
   exit(1);
 }
--- user/secret.c
+++ user/secret.c
@@ -11,10 +11,10 @@ main(int argc, char *argv[])
     printf("Usage: secret the-secret\n");
     exit(1);
   }
-  char *end = sbrk(PGSIZE*32);
+  char *end = sbrk(PGSIZE*32); // 請求了 32 頁的空間，回傳值是呼叫 sbrk() 之前的 heap 結尾，是你可以開始使用的記憶體的起始位址
   end = end + 9 * PGSIZE;
   strcpy(end, "my very very very secret pw is:   ");
-  strcpy(end+32, argv[1]);
+  strcpy(end+32, argv[1]); // secret 是寫在 sbrk() 回傳的位址 + 9 頁 + 32 bytes
   exit(0);
 }
 
